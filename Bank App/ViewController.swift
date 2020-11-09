6e14f045c80b91490ce46df8d4da643746489882
Victor Zhukov <vzmsk@mail.ru>
2020-11-09 14:01:32 +0300

    Added amount and balance labels

---
+++ Bank App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank App
+//
+//  Created by Виктор Жуков on 09.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
